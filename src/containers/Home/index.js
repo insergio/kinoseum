71dc6a1f3bb55b72af02a4b769da89a2087c3ccd
sdelahoz <sdelahoz@formax.cl>
2018-06-26 17:38:49 -0500

    WIP autosuggest

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -6,6 +6,31 @@ import SearchInput from '../../components/SearchInput';
 import Card from '../Card';
 import axios from 'axios'
 
+import Autosuggest from 'react-autosuggest';
+
+const languages = [
+  {
+    name: 'C',
+    year: 1972
+  },
+  {
+    name: 'Elm',
+    year: 2012
+  },
+  
+];
+
+
+
+
+const getSuggestionValue = suggestion => suggestion.original_title;
+
+
+const renderSuggestion = suggestion => (
+  <div>
+    {suggestion.original_title}
+  </div>
+);
 
 
 class Home extends Component {
@@ -14,8 +39,41 @@ class Home extends Component {
     value: "",
     description: "pending",
     poster: "http://placehold.it/185x277",
-    name: "asdf"
+    name: "asdf",
+
+    test: [],
+    value: "",
+    suggestions: []
   }
+
+  getSuggestions = (value) => {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+
+    return inputLength === 0 ? [] : this.state.test.filter(movie =>
+      movie.original_title.toLowerCase().slice(0, inputLength) === inputValue
+    );
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+  };
+
+
 
 
   componentDidMount(){
@@ -23,7 +81,14 @@ class Home extends Component {
     axios.get(`https://api.themoviedb.org/3/search/movie?api_key=06e590e3160fe2ade8df4051574e71f2&language=es-ES&query=fight%20club&page=1&include_adult=true`)
     .then(res => {
       console.log(res.data.results[0])
+      console.log(res.data.results)
+      
       this.setMovie(res.data.results[0])
+      
+      this.setState({
+        test: res.data.results
+      })
+   
     })
 
   }
@@ -75,6 +140,16 @@ class Home extends Component {
   
   
   render() {
+
+    const { value, suggestions } = this.state;
+
+
+    const inputProps = {
+      placeholder: 'Type a programming language',
+      value,
+      onChange: this.onChange
+    };
+
     return (
       <div className="App">
         <header className="App-header">
@@ -83,6 +158,15 @@ class Home extends Component {
           <h3>Ingresa una película para empezar</h3>
         </header>
         <SearchInput onWrite={this.handleWrite} onSubmit={this.handleSubmit}/>
+
+        <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
 
         <Card 
           name={this.state.name}
